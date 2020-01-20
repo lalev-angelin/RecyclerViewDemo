ce3a1a6b2362e97fc68eabbc6d5ae90a095266a9
Angelin Lalev <lalev@uni-svishtov.bg>
2020-01-20 17:26:41 +0200

    Working on the viewholder

--- app/src/main/java/bg/uni_svishtov/bi2016/recyclerviewdemo/DataAdapter.java
+++ app/src/main/java/bg/uni_svishtov/bi2016/recyclerviewdemo/DataAdapter.java
@@ -1,11 +1,25 @@
 package bg.uni_svishtov.bi2016.recyclerviewdemo;
 
+import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 public class DataAdapter extends RecyclerView.Adapter {
+
+    class ContactsViewHolder extends RecyclerView.ViewHolder {
+        private TextView sipTextView;
+        private TextView nameTextView;
+
+        public ContactsViewHolder(@NonNull View itemView) {
+            super(itemView);
+            sipTextView = itemView.findViewById(R.id.sipTextView);
+            nameTextView = itemView.findViewById(R.id.nameTextView);
+        }
+    }
+
     @NonNull
     @Override
     public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
